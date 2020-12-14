b9e8f0a7b34e445f206ee96b33882debcca35c6b
Higaharu <e205741@ie.u-ryukyu.ac.jp>
2020-12-14 14:51:34 +0900

    fix step1 error

--- app/src/main/java/jp/ac/uryukyu/ie/e205741/LivingThing.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205741/LivingThing.java
@@ -6,15 +6,48 @@ public class LivingThing {
     private int attack;
     private boolean dead;
 
-    
-
-    public boolean isDead(){
-        return this.dead;
+     /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * @param name モンスター名
+     * @param maximumHP モンスターのHP
+     * @param attack モンスターの攻撃力
+     */
+    public LivingThing (String name, int maximumHP, int attack) {
+        this.name = name;
+        hitPoint = maximumHP;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
 
+    
     public String getName(){
         return this.name;
     }
+    public int getHitPoint(){
+        return this.hitPoint;
+    }
+    public int getAttack(){
+        return this.attack;
+    }
+    public boolean isDead(){
+        return this.dead;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setHitPoint(int hitPoint){
+        this.hitPoint = hitPoint;
+    }
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+    public void setDead(boolean dead){
+        this.dead = dead;
+    }
+
+
 
     public void attack(LivingThing opponent){
         if(hitPoint > 0){
@@ -32,4 +65,5 @@ public class LivingThing {
         }
     }
 }
+
 
